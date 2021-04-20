426013a40c8891679717889f1ddc21aa2bdd0b9c
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-04-20 16:36:42 -0400

    Adding Location object and adding expected params

---
+++ app/src/main/java/com/dimarco/warehouseinventorysystem/Location.kt
@@ -0,0 +1,12 @@
+package com.dimarco.warehouseinventorysystem
+
+
+class Location {
+    var id: Int? = null
+    var aisle: Int? = null
+    var xCoordinate: Int? = null
+    var yCoordinate: Int? = null
+    var occupied: Boolean? = null
+    var blocked: Boolean? = null
+
+}
