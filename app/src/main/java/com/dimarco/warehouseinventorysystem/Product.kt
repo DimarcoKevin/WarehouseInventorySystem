c998b3d48a349c3f64f2e09340f6ca888d722d9f
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-04-20 16:42:55 -0400

    Adding Product class and expected params

---
+++ app/src/main/java/com/dimarco/warehouseinventorysystem/Product.kt
@@ -0,0 +1,14 @@
+package com.dimarco.warehouseinventorysystem
+
+class Product {
+    var id: Int? = null
+    var barcode: Int? = null
+
+    var minPerLocation: Int? = null
+    var maxPerLocation: Int? = null
+
+    var name: String? = null
+    var description: String? = null
+
+
+}
