a4ca156e60d76d15b2c9325a6ad5ca1236e39bb8
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-04-21 19:10:38 -0400

    Adding location object constructor

--- app/src/main/java/com/dimarco/warehouseinventorysystem/Location.kt
+++ app/src/main/java/com/dimarco/warehouseinventorysystem/Location.kt
@@ -4,13 +4,25 @@ package com.dimarco.warehouseinventorysystem
 class Location {
     var id: Int? = null
     var aisle: Int? = null
+
     var xCoordinate: Int? = null
     var yCoordinate: Int? = null
+
     var packageId: Int? = null
     var amountInLocation: Int? = null
+
     var occupied: Boolean? = null
     var blocked: Boolean? = null
 
-
+    constructor(id: Int, aisle: Int, x: Int, y: Int, packageId: Int, amount: Int, occupied: Boolean, blocked: Boolean) {
+        this.id = id
+        this.aisle = aisle
+        this.xCoordinate = x
+        this.yCoordinate = y
+        this.packageId = packageId
+        this.amountInLocation = amount
+        this.occupied = occupied
+        this.blocked = blocked
+    }
 
 }
