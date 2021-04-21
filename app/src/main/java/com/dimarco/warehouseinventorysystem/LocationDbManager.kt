d789a75e671f7957bc5c69cee86f38d581e0dccd
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-04-20 20:19:44 -0400

    Adding Database helper and context constructor

--- app/src/main/java/com/dimarco/warehouseinventorysystem/LocationDbManager.kt
+++ app/src/main/java/com/dimarco/warehouseinventorysystem/LocationDbManager.kt
@@ -1,7 +1,46 @@
 package com.dimarco.warehouseinventorysystem
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
 class LocationDbManager {
     val dbVersion = 1
     val dbName = "WarehouseInventorySystem"
     val dbTable = "Location"
+
+    val id = "id"
+
+
+    /**
+     * creating database variable
+     * creating sql table
+     */
+    var sqlDB: SQLiteDatabase? = null
+    val sqlCreateTable = "CREATE TABLE IF NOT EXISTS $dbTable ($id INTEGER PRIMARY KEY AUTOINCREMENT);"
+
+    /**
+     * constructor to create a readable/writeable database
+     */
+    constructor(context: Context) {
+        val db = DatabaseHelperLocation(context)
+        sqlDB = db.writableDatabase
+    }
+
+    /**
+     * Location database helper
+     * used to create and upgrade tables for new versions of the application
+     */
+    inner class DatabaseHelperLocation(context: Context) :
+        SQLiteOpenHelper(context, dbName, null, dbVersion) {
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL(sqlCreateTable)
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS $dbTable")
+        }
+
+    }
 }
